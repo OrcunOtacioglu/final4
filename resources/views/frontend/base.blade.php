a070fb8e73b476ce6106115fa5d7f3daef02faf2
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-10-28 13:25:44 +0300

    Added Froala_blocks

--- resources/views/frontend/base.blade.php
+++ resources/views/frontend/base.blade.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
     <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/froala_blocks.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     @yield('custom.css')
 
@@ -23,6 +24,7 @@
 
     @yield('content')
 
+    @yield('custom.html')
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="fdb-block footer-small bg-detur">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 col-md-8">
+                © 2017 Detur Official Travel Agency. All Rights Reserved
+            </div>
+
+            <div class="col-12 col-md-4 mt-4 mt-md-0 text-center text-md-right">
+                Powered by AçıkGişe
+            </div>
+        </div>
+    </div>
+</footer>
