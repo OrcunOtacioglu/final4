5f52cf82eac0aa4d331929200dc83d340436dbbe
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-10-31 14:30:19 +0300

    Added Header and Footer to the pages

---
+++ resources/views/frontend/partials/header.blade.php
@@ -0,0 +1,12 @@
+<div class="header">
+    <div class="header-wrapper">
+        @include('frontend.partials.transparent-navbar')
+        <div class="package-wrapper">
+            <div class="text-center">
+                <img src="{{ asset('img/euroleague.png') }}" alt="" height="55" class="mb-10">
+            </div>
+            <h1 class="text-center">2018 Turkish Airlines EuroLeague Final Four Belgrade</h1>
+            <h2 class="text-center mb-0">Official Travel Packages</h2>
+        </div>
+    </div>
+</div>
