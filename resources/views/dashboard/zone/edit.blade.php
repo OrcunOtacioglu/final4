3ce61136ae17f71e50ddaa5e22577ee079eec1cb
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-11-08 10:50:57 +0300

    Modifications on Zone edit page

--- resources/views/dashboard/zone/edit.blade.php
+++ resources/views/dashboard/zone/edit.blade.php
@@ -13,80 +13,14 @@
         <div class="col-md-9" id="canvasZone">
             <canvas id="{{ $zone->id }}"></canvas>
         </div>
-        <div class="col-md-3">
-            <form action="{{ action('ZoneController@addSeats', ['id' => $zone->id]) }}" method="POST">
-                {{ csrf_field() }}
-
-                <div class="form-group">
-                    <label for="direction">Direction</label>
-                    <select name="direction" id="direction" class="form-control">
-                        <option value="0">Left to Right</option>
-                        <option value="1">Right to Left</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="left">Left Start Point</label>
-                    <input type="text" name="left" id="left" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="top">Top Start Point</label>
-                    <input type="text" name="top" id="top" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="row">Row</label>
-                    <input type="text" name="row" id="row" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="start_number">Starting Seat Number</label>
-                    <input type="text" name="start_number" id="start_number" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="seat_count">Number of Seats to Add</label>
-                    <input type="text" name="seat_count" id="seat_count" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label for="rate_id">Rate</label>
-                    <select name="rate_id" id="rate_id" class="form-control">
-                        <option value="0">None</option>
-                        @foreach($rates as $rate)
-                            <option value="{{ $rate->id }}">{{ $rate->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="status">Seat Status</label>
-                    <select name="status" id="status" class="form-control">
-                        <option value="0">Not Available</option>
-                        <option value="1">Available</option>
-                        <option value="2">Online Available</option>
-                        <option value="3">Box Office Available</option>
-                        <option value="4">Booked</option>
-                    </select>
-                </div>
-
-                <input type="submit" class="btn btn-secondary" value="Add Seats">
-            </form>
-            <form action="{{ action('ZoneController@addZoneName', ['zone' => $zone->id]) }}" method="POST">
-                {{ csrf_field() }}
-
-                <div class="form-group">
-                    <label for="zone_rate">View Name</label>
-                    <select name="zone_rate" id="zone_rate" class="form-control">
-                        @foreach($rates as $rate)
-                            <option value="{{ $rate->id }}">{{ $rate->name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-
-                <input type="submit" class="btn btn-dashboard" value="Add View Name">
-            </form>
+        <div class="col-md-3" style="background: #EEEEEE;">
+            @include('dashboard.zone.partials.add-seats')
+            <hr>
+            @include('dashboard.zone.partials.zone-name')
+            <hr>
+            @include('dashboard.zone.partials.add-court')
+            <hr>
+            @include('dashboard.zone.partials.add-row')
         </div>
     </div>
 @stop
---
+++ resources/views/dashboard/zone/partials/add-seats.blade.php
@@ -0,0 +1,60 @@
+<h4>Add Seats</h4>
+<form action="{{ action('ZoneController@addSeats', ['id' => $zone->id]) }}" method="POST">
+    {{ csrf_field() }}
+
+    <div class="form-group">
+        <label for="direction">Direction</label>
+        <select name="direction" id="direction" class="form-control">
+            <option value="0">Left to Right</option>
+            <option value="1">Right to Left</option>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="left">Left Start Point</label>
+        <input type="text" name="left" id="left" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="top">Top Start Point</label>
+        <input type="text" name="top" id="top" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="row">Row</label>
+        <input type="text" name="row" id="row" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="start_number">Starting Seat Number</label>
+        <input type="text" name="start_number" id="start_number" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="seat_count">Number of Seats to Add</label>
+        <input type="text" name="seat_count" id="seat_count" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <label for="rate_id">Rate</label>
+        <select name="rate_id" id="rate_id" class="form-control">
+            <option value="0">None</option>
+            @foreach($rates as $rate)
+                <option value="{{ $rate->id }}">{{ $rate->name }}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="status">Seat Status</label>
+        <select name="status" id="status" class="form-control">
+            <option value="0">Not Available</option>
+            <option value="1">Available</option>
+            <option value="2">Online Available</option>
+            <option value="3">Box Office Available</option>
+            <option value="4">Booked</option>
+        </select>
+    </div>
+
+    <input type="submit" class="btn btn-block btn-dashboard" value="Add Seats">
+</form>
