cc7ce223d96feeeabec6a3c650beef7b84449826
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-11-10 16:58:31 +0300

    Added Terms and Conditions

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -11,26 +11,26 @@
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-sm-left">
                 <h3><strong>Terms & Conditions</strong></h3>
                 <nav class="nav flex-column">
-                    <a class="nav-link" href="https://www.froala.com">Terms & Conditions</a>
-                    <a class="nav-link" href="https://www.froala.com">Disclaimer</a>
-                    <a class="nav-link" href="https://www.froala.com">Cookies Policy</a>
-                    <a class="nav-link" href="https://www.froala.com">FAQ</a>
+                    <a class="nav-link" href="http://events.detur.com/terms-conditions">Terms & Conditions</a>
+                    <a class="nav-link" href="">Disclaimer</a>
+                    <a class="nav-link" href="">Cookies Policy</a>
+                    <a class="nav-link" href="">FAQ</a>
                 </nav>
             </div>
 
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 mt-5 mt-sm-0 text-sm-left">
                 <h3><strong>Company Profile</strong></h3>
                 <nav class="nav flex-column">
-                    <a class="nav-link active" href="https://www.froala.com">About Us</a>
-                    <a class="nav-link" href="https://www.froala.com">Contact</a>
+                    <a class="nav-link active" href="">About Us</a>
+                    <a class="nav-link" href="">Contact</a>
                 </nav>
             </div>
 
             <div class="col-12 col-lg-2 ml-auto text-lg-left mt-4 mt-lg-0">
                 <h3><strong>Follow Us</strong></h3>
                 <p class="text-h3">
-                    <a href="https://www.froala.com"><i class="ti-twitter-alt" aria-hidden="true"></i></a>&nbsp;&nbsp;
-                    <a href="https://www.froala.com"><i class="ti-facebook" aria-hidden="true"></i></a>&nbsp;&nbsp;
+                    <a href=""><i class="ti-twitter-alt" aria-hidden="true"></i></a>&nbsp;&nbsp;
+                    <a href=""><i class="ti-facebook" aria-hidden="true"></i></a>&nbsp;&nbsp;
                 </p>
             </div>
         </div>
