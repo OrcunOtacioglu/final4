938575371867c4ab9abca804ac4599d957ca3b98
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-11-17 09:57:37 +0300

    Basic analytics added

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,4 +1,116 @@
 @extends('dashboard.base')
 
+@section('title', 'Dashboard')
+
 @section('content')
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card card-block p-30 bg-blue-600">
+                <div class="card-watermark darker font-size-80 m-15"><i class="icon ti-receipt" aria-hidden="true"></i></div>
+                <div class="counter counter-md counter-inverse text-left">
+                    <div class="counter-number-group">
+                        <span class="counter-number">{{ $sales->count() }}</span>
+                        <span class="counter-number-related text-capitalize">orders</span>
+                    </div>
+                    <div class="counter-label text-capitalize">in total</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card card-block p-30 bg-red-600">
+                <div class="card-watermark darker font-size-80 m-15"><i class="icon ti-money" aria-hidden="true"></i></div>
+                <div class="counter counter-md counter-inverse text-left">
+                    <div class="counter-number-group">
+                        <span class="counter-number">{{ \App\Entities\Sale::getTotalSalesAmount() }}€</span>
+                        <span class="counter-number-related text-capitalize">sales</span>
+                    </div>
+                    <div class="counter-label text-capitalize">in total</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card card-block p-30 bg-green-600">
+                <div class="card-watermark darker font-size-80 m-15"><i class="icon ti-user" aria-hidden="true"></i></div>
+                <div class="counter counter-md counter-inverse text-left">
+                    <div class="counter-number-group">
+                        <span class="counter-number">{{ $customers->count() }}</span>
+                        <span class="counter-number-related text-capitalize">customers</span>
+                    </div>
+                    <div class="counter-label text-capitalize">in total</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="panel">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Hotel Availabilities</h2>
+                </div>
+                <div class="panel-body">
+                    <table id="hotelTable" class="table">
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Remaining Availabilty</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($hotels as $hotel)
+                            <tr>
+                                <td>{{ $hotel->name }}</td>
+                                <td>{{ $hotel->online_availability }}</td>
+                                <td>
+                                    <a href="{{ action('HotelController@edit', ['id' => $hotel->id]) }}" class="btn btn-sm btn-icon btn-flat btn-default" data-toggle="tooltip" data-original-title="Edit">
+                                        <i class="icon ti-pencil"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="panel">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Latest Sales</h2>
+                    <ul class="panel-actions panel-actions-keep">
+                        <li><a href="#">See All Sales</a></li>
+                    </ul>
+                </div>
+                <div class="panel-body">
+                    <table id="saleTable" class="table">
+                        <thead>
+                        <tr>
+                            <th>Reference</th>
+                            <th>User Email</th>
+                            <th>Total Amount</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($sales as $sale)
+                            <tr>
+                                <td>{{ $sale->reference }}</td>
+                                <td>{{ $sale->user->email }}</td>
+                                <td>{{ $sale->total }}€</td>
+                                <td>
+                                    <div class="row">
+                                        <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#{{ $sale->reference }}">
+                                            <i class="icon ti-eye"></i> View Details
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    @include('dashboard.entities.sale.partials.details')
 @stop
