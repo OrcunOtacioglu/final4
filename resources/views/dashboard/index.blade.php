8843310f3b4f459e15c758d3d0d4b6b89ddd28cd
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-12-01 15:31:26 +0300

    Added money formatter

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -25,7 +25,7 @@
                 <div class="card-watermark darker font-size-80 m-15"><i class="icon ti-money" aria-hidden="true"></i></div>
                 <div class="counter counter-md counter-inverse text-left">
                     <div class="counter-number-group">
-                        <span class="counter-number">{{ money_format('%i',\App\Entities\Sale::getTotalSalesAmount()) }}€</span>
+                        <span class="counter-number">{{ \Acikgise\Helpers\Helpers::formatMoney(\App\Entities\Sale::getTotalSalesAmount()) }}</span>
                         <span class="counter-number-related text-capitalize">sales</span>
                     </div>
                     <div class="counter-label text-capitalize">in total</div>
