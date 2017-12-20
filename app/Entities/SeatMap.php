be2634f1913cc0713cf3bc85edfa752a2ba05b9c
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2017-12-20 12:03:03 +0300

    Added create and update functionalities on Venue and SeatMap entities

--- app/Entities/SeatMap.php
+++ app/Entities/SeatMap.php
@@ -2,7 +2,10 @@
 
 namespace App\Entities;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SeatMap extends Model
 {
@@ -21,5 +24,31 @@ class SeatMap extends Model
     public function zones()
     {
         return $this->hasMany(Zone::class);
+    }
+
+    public static function createNew(Request $request)
+    {
+        $seatmap = new SeatMap();
+
+        $seatmap->venue_id = $request->venue_id;
+
+        $seatmap->created_at = Carbon::now();
+        $seatmap->updated_at = Carbon::now();
+
+        $seatmap->save();
+
+        return $seatmap;
+    }
+
+    public static function updateEntity(Request $request, $id)
+    {
+        $seatmap = SeatMap::findOrFail($id);
+
+        $seatmap->venue_id = $request->venue_id;
+        $seatmap->updated_at = Carbon::now();
+
+        $seatmap->save();
+
+        return $seatmap;
     }
 }
--- app/Entities/Venue.php
+++ app/Entities/Venue.php
@@ -2,7 +2,9 @@
 
 namespace App\Entities;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Venue extends Model
 {
@@ -27,5 +29,48 @@ class Venue extends Model
     public function seatmaps()
     {
         return $this->hasMany(SeatMap::class);
+    }
+
+    public static function createNew(Request $request)
+    {
+        $venue = new Venue();
+
+        $venue->name = $request->name;
+
+        $venue->postal_code = $request->postal_code;
+        $venue->timezone = $request->timezone;
+        $venue->city = $request->city;
+        $venue->country = $request->country;
+        $venue->address = $request->address;
+        $venue->longitude = $request->longitude;
+        $venue->latitude = $request->latitude;
+
+        $venue->created_at = Carbon::now();
+        $venue->updated_at = Carbon::now();
+
+        $venue->save();
+
+        return $venue;
+    }
+
+    public static function updateEntity(Request $request, $id)
+    {
+        $venue = Venue::findOrFail($id);
+
+        $venue->name = $request->name;
+
+        $venue->postal_code = $request->postal_code;
+        $venue->timezone = $request->timezone;
+        $venue->city = $request->city;
+        $venue->country = $request->country;
+        $venue->address = $request->address;
+        $venue->longitude = $request->longitude;
+        $venue->latitude = $request->latitude;
+
+        $venue->updated_at = Carbon::now();
+
+        $venue->save();
+
+        return $venue;
     }
 }
