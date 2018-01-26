ac9ed0ec09aa67102ec7616a490bd5d0e92da9cc
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2018-01-26 18:14:49 +0300

    New Dashboard HomePage

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,3 +1,54 @@
 @extends('layouts.dashboard')
 
 @section('title', 'Dashboard')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="panel panel-success panel-line">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">TOTAL SALES</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p class="text-success font-size-40 mb-0">0.00 EUR</p>
+                            <p>30 Days</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="panel panel-warning panel-line">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">ORDERS COMPLETED</h3>
+                        </div>
+                        <div class="panel-body">
+                            <p class="text-warning font-size-40 mb-0">0</p>
+                            <p>30 Days</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel">
+                <div class="panel-heading">
+                    <h3 class="panel-title">QUICK LINKS</h3>
+                </div>
+                <div class="panel-body">
+                    <div class="list-group">
+                        <a class="list-group-item" href="{{ action('EventController@create') }}">
+                            <i class="icon ti-plus"></i>Create New Event
+                        </a>
+                        <a class="list-group-item" href="javascript:void(0)">
+                            <i class="icon ti-ticket"></i>Manage/Edit Events
+                        </a>
+                        <a class="list-group-item" href="javascript:void(0)">
+                            <i class="icon ti-home"></i>Manage/Edit Hotels
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta name="description" content="AçıkGişe Ticketing Solutions Software">
     <meta name="author" content="Orçun Otacıoğlu">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @yield('custom.meta')
 
     <title>@yield('title', 'Dashboard') - AçıkGişe Ticketing Solutions</title>
@@ -35,16 +36,35 @@
     <!-- End Site Navbar -->
 
     <!-- Site Sidebar -->
-    @include('dashboard.partials.sidebar')
+    {{--@include('dashboard.partials.sidebar')--}}
     <!-- End Site Sidebar -->
 
     <!-- Page -->
-    <div class="page" id="#app">
-        <div class="page-content container-fluid">
+    <div class="page" id="dashboard">
+        <div class="page-header @yield('container.type', 'container-fluid')">
+            <h1 class="page-title">@yield('title')</h1>
+            @yield('page-description')
+            {{--<ol class="breadcrumb">--}}
+            {{--<li class="breadcrumb-item"><a href="../index.html">Home</a></li>--}}
+            {{--<li class="breadcrumb-item active">You are Here</li>--}}
+            {{--</ol>--}}
+            <div class="page-header-actions">
+                <div class="btn-group btn-group-sm" id="withBtnGroup" aria-label="Header Actions" role="group">
+                    {{--<button type="button" class="btn btn-outline btn-default">--}}
+                    {{--<i class="icon wb-wrench" aria-hidden="true"></i>--}}
+                    {{--<span class="hidden-sm-down">Settings</span>--}}
+                    {{--</button>--}}
+                    @yield('header.actions')
+                </div>
+            </div>
+        </div>
+        <div class="page-content @yield('container.type', 'container-fluid')">
             @yield('content')
         </div>
     </div>
     <!-- End Page -->
+
+    @yield('custom.html')
 
     <!-- Footer -->
     @include('dashboard.partials.footer')
@@ -60,5 +80,9 @@
     @include('dashboard.partials.js.config')
     <!-- Page -->
     @include('dashboard.partials.js.page')
+    <!-- Dashboard Instance -->
+    <script src="{{ asset('admin/js/core/dashboard.js') }}"></script>
+    <!-- Custom -->
+    @yield('footer.scripts')
 </body>
 </html>
