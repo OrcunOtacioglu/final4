37e89145ee3121f6f6a416d0a32e4a28fc4325ad
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2018-02-09 11:56:04 +0300

    Added sweetalert asset

--- resources/views/frontend/assets/global-js.blade.php
+++ resources/views/frontend/assets/global-js.blade.php
@@ -2,4 +2,5 @@
 <script src="{{ asset('frontend/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('frontend/js/jquery-ui-1.10.4.min.js') }}"></script>
 <script src="{{ asset('frontend/js/functions.js') }}"></script>
+<script src="{{ asset('frontend/js/plugins/sweetalert.min.js') }}"></script>
 <script src="{{ asset('frontend/js/app.js') }}"></script>
